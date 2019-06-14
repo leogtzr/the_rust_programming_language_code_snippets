b29c414dcfcc717173f1b7cbc18c7ed9bcb32256
leogtzr <leogutierrezramirez@gmail.com>
2019-06-14 10:35:03 -0600

    add some code example about method syntax (rectangle example)

--- rectangles1/src/main.rs
+++ rectangles1/src/main.rs
@@ -1,18 +1,50 @@
-fn area(witdh: u32, height: u32) -> u32 {
-	witdh * height
+fn area(width: u32, height: u32) -> u32 {
+	width * height
 }
 
 fn area2(rect: (u32, u32)) -> u32 {
 	rect.0 * rect.1
 }
 
+fn area3(rect: &Rectangle) -> u32 {
+	rect.width * rect.height
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+	fn area(&self) -> u32 {
+		self.width * self.height
+	}
+}
+
 fn main() {
-    // let witdh1 = 30;
+    // let width1 = 30;
     // let height1 = 50;
 
-    // println!("The area of the rectangle is {} square pixels.", area(witdh1, height1));
+    // println!("The area of the rectangle is {} square pixels.", area(width1, height1));
 
     let rect1 = (30, 50);
     println!("The area of the rectangle is {} square pixels.", area2(rect1));
+
+    let rect2 = Rectangle  {
+    	width: 30,
+    	height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area3(&rect2)
+    );
+
+    println!("{:#?}", rect2);
+
+    println!("~~~~~~~~~~~~~~~~~~~~~~>");
+
+    println!("The area is: {}", rect2.area());
 
 }
