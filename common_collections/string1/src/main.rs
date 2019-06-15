83f1e6956a4a4c0fc95170f4f51d354f0770837d
leogtzr <leogutierrezramirez@gmail.com>
2019-06-14 22:19:23 -0600

    add some string code

---
+++ common_collections/string1/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut s = String::new();
+    s.push_str("asd");
+
+    // let s2: String = s.to_string();
+    let data = "initial contents";
+    let s2: String = data.to_string();
+
+    let s3 = "Leonardo".to_string();
+}
